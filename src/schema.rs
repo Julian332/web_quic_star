bf65a3407850a47ec236c446db0e633b51b81385
jinguocao332@gmail.com <jinguocao332@gmail.com>
2025-04-09 15:55:05 +0800

    feature: permission

--- src/schema.rs
+++ src/schema.rs
@@ -15,26 +15,6 @@ diesel::table! {
 }
 
 diesel::table! {
-    groups_permissions (group_id, permission_id) {
-        group_id -> Int8,
-        permission_id -> Int8,
-    }
-}
-
-diesel::table! {
-    permissions (id) {
-        id -> Int8,
-        name -> Text,
-        remark -> Nullable<Text>,
-        update_time -> Nullable<Timestamptz>,
-        create_time -> Timestamptz,
-        create_by -> Int8,
-        update_by -> Nullable<Int8>,
-        is_delete -> Bool,
-    }
-}
-
-diesel::table! {
     users (id) {
         id -> Int8,
         username -> Text,
